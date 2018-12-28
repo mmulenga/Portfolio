bdfa8c9ddae0b680c498772d56ab3745a604291c
mmulenga <matt.mulengawoo@gmail.com>
2018-12-27 23:12:30 -0600

    Adding purely renderable components.

---
+++ src/components/NavButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavButton() {
+  return (
+    <button type="button">
+      <object data="placeholder" type="image/svg+xml" aria-label="placeholder" />
+    </button>
+  );
+}
+
+export default NavButton;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavButton from './NavButton';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <NavButton />
+      <NavButton />
+      <NavButton />
+      <NavButton />
+    </div>
+  );
+}
+
+export default Navbar;
