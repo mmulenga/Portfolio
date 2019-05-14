526774bf3d55360f7baffbd19849754f6023f63d
mmulenga <matt.mulengawoo@gmail.com>
2019-05-14 14:50:52 -0600

    Added tablet support.

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -6,7 +6,7 @@ function Name() {
   return (
     <div className="NameContainer">
       <h1 className="Name"> Matt Mulenga </h1>
-      <ColouredLine />
+      <ColouredLine position="Center" />
       <h2 className="Title"> Software Developer | B. Sc. </h2>
     </div>
   );
