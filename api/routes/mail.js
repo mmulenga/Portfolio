e28e213cbccf6e19707edc9018021fce2e26f20d
mmulenga <matt.mulengawoo@gmail.com>
2019-06-13 14:28:00 -0600

    Implemented nodemailer functionality.

---
+++ api/routes/mail.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+
+const credentials = require('../config/config');
+
+var transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  auth: {
+    user: credentials.USER,
+    pass: credentials.PASS,
+  }
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server ready to accecpt emails.');
+  }
+})
+
+router.post('/', (req, res, next) => {
+  var email = req.body.email;
+  var message = req.body.message;
+
+  var mail = {
+    from: email,
+    to: 'matt.mulengawoo@gmail.com',
+    subject: 'Portfolio Site Contact',
+    text: message
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: 'fail'
+      });
+    } else {
+      res.json({
+        msg: 'success'
+      });
+    }
+  });
+});
+
+module.exports = router;
--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,6 +1,8 @@
 // @flow
 
 import * as React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './Contact.css';
 
 type Props = {
@@ -9,17 +11,46 @@ type Props = {
 
 function Contact(props: Props) {
   const { colorScheme } = props;
-  const scheme = { backgroundColor: colorScheme };
+  const [senderEmail, setSenderEmail] = useState('');
+  const [emailBody, setEmailBody] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios({
+      method: 'POST',
+      url: 'http://localhost:9000/',
+      data: {
+        email: senderEmail,
+        message: emailBody,
+      }
+    }).then((response) => {
+      if (response.data.msg === 'success') {
+        alert('Message sent.');
+      } else {
+        alert('Message failed to send.');
+      }
+    })
+  }
 
   return (
-    <form className="contact">
+    <form className="contact" onSubmit={(e) => handleSubmit(e)} method="POST">
       <p className="contact-text">Your email address</p>
-      <input className="sender-email" type="text" />
+      <input
+        className="sender-email"
+        type="text"
+        value={senderEmail}
+        onChange={(e) => setSenderEmail(e.target.value)}
+      />
       <p className="contact-text">Message</p>
-      <textarea className="email-box" />
+      <textarea
+        className="email-box"
+        value={emailBody}
+        onChange={(e) => setEmailBody(e.target.value)}
+      />
       <input
         className="send-button"
-        style={scheme}
+        style={{ backgroundColor: colorScheme }}
         type="submit"
         value="Send"
       />
