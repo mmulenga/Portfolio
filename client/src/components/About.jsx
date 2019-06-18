b20bc647891c7d4a3eba415b887898105ec2c1ac
mmulenga <matt.mulengawoo@gmail.com>
2019-06-18 17:35:30 -0600

    Edited about.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,10 +8,10 @@ function About() {
     <div className="about-text">
       Inspired by all things tech, I grew up to develop a passion for programming.
       As an aspiring full-stack software engineer I&apos;ve taken an interest in learning
-      a wide variety of subjects, ranging from front-end technologies, like React, to
-      back-end technologies like PostgreSQL.
+      a wide variety of subjects, ranging from front-end technologies like React (This
+      website was built with React!), to back-end technologies like PostgreSQL.
       When I&apos;m not programming you can find me playing video-games, planning my next
-      trip around the world or just flat out enjoying the outdoors. If I sound like the
+      trip around the world, or just flat out enjoying the outdoors. If I sound like the
       kind of person you&apos;re looking for to join your team, give me a shout below!
     </div>
   );
