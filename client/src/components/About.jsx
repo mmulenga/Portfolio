445626efdea7657338a96e8d17c78e5e8735487c
mmulenga <matt.mulengawoo@gmail.com>
2019-07-12 15:26:19 -0600

    Added tooltips.

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,36 +1,23 @@
 // @flow
 
-import  * as React from 'react';
-import { useState } from 'react';
-import Tooltip from './Tooltip';
-import Portal from './Portal';
+import * as React from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
 import './About.css';
 
 function About() {
-  const [tooltipVisibility, setTooltipVisibility] = useState({ visibility: 'hidden' });
-
   return (
-    <React.Fragment>
-      <div className="about-text">
-        Inspired by all things tech, I grew up to develop a passion for programming.
-        As an aspiring full-stack software engineer I&apos;ve taken an interest in learning
-        a wide variety of subjects, ranging from front-end technologies like&nbsp;
-        <span
-          className="underline"
-          onMouseEnter={() => setTooltipVisibility({ visibility: 'visible' })}
-          onMouseLeave={() => setTooltipVisibility({ visibility: 'hidden' })}
-        >
-        React
-        </span>
-         , to back-end technologies like PostgreSQL.
-        When I&apos;m not programming you can find me playing video-games, planning my next
-        trip around the world, or just flat out enjoying the outdoors. If I sound like the
-        kind of person you&apos;re looking for to join your team, give me a shout below!
-      </div>
-      <Portal>
-        <Tooltip style={tooltipVisibility}> This site was created using React! </Tooltip>
-      </Portal>
-    </React.Fragment>
+    <div className="about-text">
+      Inspired by all things tech, I grew up to develop a passion for programming.
+      As an aspiring full-stack software engineer I&apos;ve taken an interest in learning
+      a wide variety of subjects, ranging from front-end technologies like&nbsp;
+      <Tooltip title="This site was created using React!" placement="top">
+        <span className="underline">React</span>
+      </Tooltip>
+       , to back-end technologies like PostgreSQL.
+      When I&apos;m not programming you can find me playing video-games, planning my next
+      trip around the world, or just flat out enjoying the outdoors. If I sound like the
+      kind of person you&apos;re looking for to join your team, give me a shout below!
+    </div>
   );
 }
 
